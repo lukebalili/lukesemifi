f8889a18845e7f889efe86fac995d831dd4f2443
gundexarthal <gundex45@gmail.com>
2025-05-06 03:40:09 +0800

    fifth commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Luke S. Balili BSIT System Integration Semi-Finalsss'
+    return 'Luke S. Balili BSIT System Integration Semi-Finals'
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
